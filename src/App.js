7d10d8b5de7bb4a1970a6127bc316d4e4f8599aa
Doniele Arys Antonio <arysantonio363@gmail.com>
2024-10-28 02:14:12 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.jsx
+
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Chat from './components/Chat';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-blue-300 to-blue-200">
+      {isLoggedIn ? <Chat /> : <Login setIsLoggedIn={setIsLoggedIn} />}
     </div>
   );
 }
